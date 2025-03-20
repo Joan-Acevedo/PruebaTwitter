0d64aa4552f1f2d60dc6e4b55b1a7b31e087fe9c
joan.acevedo <joan.acevedo@mail.escuelaing.edu.co>
2025-03-20 18:55:24 -0500

    Espero sirva, Juan Pa y Andreita

--- src/main/java/com/example/twitter/repository/UserRepository.java
+++ src/main/java/com/example/twitter/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.example.twitter.repository;
 import com.example.twitter.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByUsername(String username); // verificar si un usuario ya existe
 }
--- src/main/java/com/example/twitter/services/UserService.java
+++ src/main/java/com/example/twitter/services/UserService.java
@@ -5,25 +5,36 @@ import com.example.twitter.model.User;
 import com.example.twitter.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
-    public UserService(UserRepository u){
-        userRepository = u;
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
-    public User createUser(User user){
+    public User registerUser(User user) {
+        if (userRepository.findByUsername(user.getUsername()).isPresent()) {
+            throw new RuntimeException("El usuario ya existe");
+        }
+
+        // Encriptar la contraseña antes de guardarla
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
         return userRepository.save(user);
-    }
-
-    public List<User> getAllUser(){
-        return userRepository.findAll();
     }
 
     public List<User> getUsers() {
         return userRepository.findAll();
     }
+
+    public boolean existsByUsername(String username) {
+         return userRepository.findByUsername(username).isPresent();
+    }
+
 }
